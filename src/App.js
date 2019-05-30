53887ac48d3110f155533ffef3f1b57cde10ad23
tuppers360 <tuppers360@live.co.uk>
2019-05-30 21:56:28 +0100

    16 - Adding Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 import "./App.css";
 
-function App() {
-  return (
-    <main className="Container">
-      <Movies />
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main className="Container">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect exact from="/" to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    );
+  }
 }
 
 export default App;

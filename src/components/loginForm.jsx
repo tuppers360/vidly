4e1cf240499e19d9fa33789acc829272c103e1c2
tuppers360 <tuppers360@live.co.uk>
2019-06-04 20:25:21 +0100

    15 - Validate fields with Joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Input from "./common/input";
+import Joi from "joi-browser";
 
 class LoginForm extends Component {
   state = {
@@ -7,16 +8,30 @@ class LoginForm extends Component {
     errors: {}
   };
 
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+
   validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
+
     const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
 
-    if (this.state.account.username.trim() === "")
-      errors.username = "Username is required";
-
-    if (this.state.account.password.trim() === "")
-      errors.password = "Password is Password";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   };
 
   handleSubmit = e => {
@@ -26,16 +41,8 @@ class LoginForm extends Component {
     this.setState({ errors: errors || {} });
     if (errors) return;
 
+    //call the server
     console.log("Submitted");
-  };
-
-  validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") return "Username is required.";
-    }
-    if (name === "password") {
-      if (value.trim() === "") return "Password is required.";
-    }
   };
 
   handleChange = ({ currentTarget: input }) => {
@@ -69,7 +76,9 @@ class LoginForm extends Component {
             onChange={this.handleChange}
             error={errors.password}
           />
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );

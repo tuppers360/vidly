e818e480ff7a2085178f92a2869ce579077a5791
tuppers360 <tuppers360@live.co.uk>
2019-06-07 20:16:57 +0100

    Add movieService

---
+++ src/services/movieService.js
@@ -0,0 +1,30 @@
+import http from "./httpService";
+
+const apiEndPoint = "http://localhost:3900/api/movies";
+
+export function getMovies() {
+  return http.get(apiEndPoint);
+}
+
+// export function getMovie(id) {
+//   return movies.find(m => m._id === id);
+// }
+
+// export function saveMovie(movie) {
+//   let movieInDb = movies.find(m => m._id === movie._id) || {};
+//   movieInDb.title = movie.title;
+//   movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
+//   movieInDb.numberInStock = movie.numberInStock;
+//   movieInDb.dailyRentalRate = movie.dailyRentalRate;
+
+//   if (!movieInDb._id) {
+//     movieInDb._id = Date.now().toString();
+//     movies.push(movieInDb);
+//   }
+
+//   return movieInDb;
+// }
+
+export function deleteMovie(movieId) {
+  return http.delete(apiEndPoint + "/" + movieId);
+}
